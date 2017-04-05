6ee4c67119cbd69de72a6eab5db312f700a634da
Chandana N T <chandanant58@gmail.com>
2017-04-05 22:08:57 +0530

    Changed to .php and added search

---
+++ parallax-template/index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+  <title>zomato</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+  <nav class="white" role="navigation">
+    <div class="nav-wrapper container">
+      <a id="logo-container" href="#" class="brand-logo">zomato</a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="#"></a></li>
+      </ul>
+
+      <ul id="nav-mobile" class="side-nav">
+        
+      </ul>
+      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
+
+  <div id="index-banner" class="parallax-container">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <br><br>
+        <h3 class="header center white-text text-lighten-2">zomato</h3>
+        <div class="col s1 center"><center><img src="heart.jpg" style="width:130px;height:120px;top=150px;"></center></div>
+        <div class="row center">
+          <h5 class="header col s12 light"><b>Find the best cafes and restaurants near you !</b></h5>
+        </div>
+
+        <div class="row">
+        
+        <form action="search.php" method="POST">
+          <input type="search" name="search_val" class="search red" placeholder="Search for what you crave ..." />
+          <center><input type="submit" class="waves-effect waves-light btn red" ></input></center>
+       </form>
+
+       <!-- <center><a class="waves-effect waves-light btn red" href="search.html">search</a></center> -->
+          
+        </div>  
+        
+        <br><br>
+
+      </div>
+    </div>
+    <div class="parallax"><img src="cover2.jpg" alt="Unsplashed background img 1"></div>
+  </div>
+ 
+  <div class="row">
+    </br>
+  </div>
+
+  <div class="row">
+    </br>
+  </div>
+
+  <div class="container">
+    <div class="section">
+
+      <!--   Icon Section   -->
+      <div class="row">
+        <div class="col s12 m4">
+         
+       
+          <div class="card">
+            <div class="card-image">
+              <img src="tile1.jpg">
+              <span class="card-title"></span>
+            </div>
+            <div class="card-content">
+              <p>Juicy burgers and fries, pancakes, waffles with ice cream, sandwhiches, submarines, hotdogs, hamburgers, sausages and yummy desserts.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">American collection</a>
+            </div>
+          </div>
+       
+            
+        </div>
+
+        <div class="col s12 m4">
+          <div class="card">
+            <div class="card-image">
+              <img src="tile2.jpg">
+              <span class="card-title"></span>
+            </div>
+            <div class="card-content">
+              <p>Dosas and Idlis from the south, Roti, Paneer and Parathas from the north, Sweets from the west,Indian food will not dissapoint.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">indian collection</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col s12 m4">
+          <div class="card">
+            <div class="card-image">
+              <img src="tile3.jpg">
+              <span class="card-title"></span>
+            </div>
+            <div class="card-content">
+              <p>Pizzas,Pastas, Foccacia sandwhiches, Bruschetta, Lasagnaes and all the other cheesy stuff will keep your mouth watering for more.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">italian collection</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+<div class="row">
+    </br>
+</div>
+
+  <div class="parallax-container valign-wrapper">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <div class="row center">
+          <h5 class="header col s12 ">You don't have to know the name of the restaurant</h5>
+     <h5 class="header col s12 ">We support search by cusine & food item</h5>
+        </div>
+      </div>
+    </div>
+    <div class="parallax"><img src="cover3.jpg" alt="Unsplashed background img 2"></div>
+  </div>
+
+  <div class="container">
+    <div class="section">
+
+      <div class="row">
+        <div class="col s12 center">
+          <h3><i class="mdi-content-send brown-text"></i></h3>
+          <h4>How we can help you out?</h4>
+          <p class="left-align light">Did you just hear about a restaurant from one of your foodie friends or family and want to find out more about it ? Just type in the name of the restaurant in our search bar to get to know about the restaurant's timings,address,cost per head and menu. But what if you're just in the mood for Nachos or some American Food and have no restaurant/cafe on your mind. Just type out the cusine you wanna try out or the food item you're craving fun and we'll figure out the best place for you. We allow restaurant owners to add their own restaurants so our website is always updated with the latest news for all the food lovers out there.</p>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+
+  <div class="parallax-container valign-wrapper">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <div class="row center">
+          <h5 class="header col s12 ">You can add your own restaurant to our site too</h5>
+    <h5 class="header col s12 ">We can grow together</h5>
+        </div>
+      </div>
+    </div>
+    <div class="parallax"><img src="footer.jpg" alt="Unsplashed background img 3"></div>
+  </div>
+
+  <footer class="page-footer red">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">About Us</h5>
+          <p class="grey-text text-lighten-4">We are a team of college students who want to make it easy for everyone to find the best places to satisfy their cravings within their budget !</p>
+
+
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">For Restaurants</h5>
+          <ul>
+            <li><a class="white-text" href="">Add a restaurant</a></li>
+            <li><a class="white-text" href="#!">Buisness owner guidelines</a></li>
+            <li><a class="white-text" href="#!">Legal Aspects</a></li>
+            <li><a class="white-text" href="#!">Get our widgets</a></li>
+          </ul>
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Locations</h5>
+          <ul>
+            <li><a class="white-text" href="https://www.google.co.in/maps/place/Mangaluru,+Karnataka/@12.9231621,74.7820487,12z/data=!3m1!4b1!4m5!3m4!1s0x3ba35a4c37bf488f:0x827bbc7a74fcfe64!8m2!3d12.9141417!4d74.8559568">Mangalore</a></li>
+            <li><a class="white-text" href="#!">We are expanding!</a></li>
+            <li><a class="white-text" href="#!"></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+      Made by <a class="brown-text text-lighten-3" href="https://github.com/kavya-a/ITA-Project">Junglee & ChandyCool</a>
+      </div>
+    </div>
+  </footer>
+
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+
+  </body>
+</html>
